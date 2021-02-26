1f5b3bded98060d3fd726bf8fb73f5b2fb9fe1e1
SADAMFAUZI <79291708+SADAMFAUZI@users.noreply.github.com>
2021-02-26 09:58:08 +0700

    [aktifias 3]_Sadam Fauzi 1905109

---
+++ aktifitas3.py
@@ -0,0 +1,13 @@
+#PERCABANGAN
+print("Percabangan pada bahasa pemograman Python")
+print("Pilih bahasa Pemograman: ")
+print("2.C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+	print("Bahasa pemograman yang dipilih adalah Python")
+elif pilih == 2:
+	print("Bahasa pemograman yang dipilih adalah C")
+else :
+	print("Pilihan Anda tidak tersedia!!!")
