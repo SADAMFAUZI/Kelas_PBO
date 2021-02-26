387434e09f313366cd4e1e8c8f647eab882e37fd
SADAMFAUZI <79291708+SADAMFAUZI@users.noreply.github.com>
2021-02-26 09:20:37 +0700

    [aktifitas1]_Sadam Fauzi 1905109

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("Masukan Nama : Sadam Fauzi")
+umur=19
+
+#OUTPUT
+print("")
+print("======INPUT DAN OUTPUT PADA BAHASA PEMOGAMAN PYTHON====")
+print("Nama Anda :", nama)
+print("Nama Anda :", umur)
+print("")
